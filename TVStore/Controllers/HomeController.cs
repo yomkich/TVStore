e61a60353932968b282870c7cd9d36b20b5bd0b5
Admin <Admin@LESHA7311>
2023-04-20 23:02:10 +0400

    Добавьте файлы проекта.

---
+++ TVStore/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TVStore.Models;
+
+namespace TVStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.TVId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+       TVContext db;
+        public HomeController(TVContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.TVs.ToList());
+        }
+    }
+}
